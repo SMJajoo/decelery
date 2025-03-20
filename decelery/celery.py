c9ae8f6e377233a5a1cff9c566bebd7be4bc96d9
SJ <psxsj9@nottingham.ac.uk>
2025-03-20 19:42:03 +0000

    adding taska in queue

--- decelery/celery.py
+++ decelery/celery.py
@@ -4,9 +4,5 @@ from celery import Celery
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "decelery.settings") # celery to access settings in django project
 app = Celery("decelery") # create a celery app
 app.config_from_object("django.conf:settings", namespace="CELERY") # load celery settings from django settings
-
-@app.task # decorator to make the function a celery task
-def add_numbers():
-    return
-
+app.conf.task_routes = {'newapp.tasks.task1': {'queue':'queue1'}, 'newapp.tasks.task2': {'queue':'queue2'}}
 app.autodiscover_tasks() # autodiscover tasks in the app
